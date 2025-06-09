8f649431db9a53d9f5cc83be8e9b4da531a426b4
Shikhar <devilshikhar123@gmail.com>
2025-06-10 00:14:29 +0530

    All setup configured

--- src/index.js
+++ src/index.js
@@ -15,15 +15,17 @@
 //         throw error
 //     }
 // })()
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path:'./env'
 })
+const app = express();
 const port=process.env.PORT || 8000
 connectDB().then(()=>{
-    app.on((error)=>{
+    app.on("error",(error)=>{
         console.log("error",error);
         throw error
     })
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import jwt from "jsonwebtoken"
 
 const userSchema = new mongoose.Schema({
         username:{
@@ -51,14 +52,15 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save",async function (next){
     if(!this.isModified("password")) return next();
-    this.password=bcrypt.hash(this.password,10)
+    this.password = bcrypt.hash(this.password,10)
     next()
 })
 userSchema.methods.isPasswordCorrect = async function(password){
-    await bcrypt.compare(password,this.password)
+    return await bcrypt.compare(password,this.password)
 }
 
-userSchema.methods.generateAccessToken =function(){
+userSchema.methods.generateAccessToken =function()
+{
     return jwt.sign(
         {
             _id:this._id,
